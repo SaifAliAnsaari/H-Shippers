4702528fcc289e88c85398fb274f39d201ebcd06
SaifAliAnsaari <saifaliansaari@gmail.com>
2019-03-04 11:14:11 +0500

    style_access_right

--- resources/views/access_rights/access_rights.blade.php
+++ resources/views/access_rights/access_rights.blade.php
@@ -14,43 +14,49 @@
     </div>
 </div>
 
-<form id="saveAccessRights">
-    {!! Form::hidden('tokenForAjaxReq', csrf_token()) !!}
-    @csrf
-    <input name="access_route" hidden type="text" />
-    <input name="select_employee" hidden type="text" value="<?= $employee_id ?>">
-    {{-- <div style="min-height: 400px" id="dataSidebarLoader" style="">
-        <img src="/images/loader.gif" width="30px" height="auto" style="position: absolute; left: 50%; top: 45%;">
-    </div> --}}
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <form id="saveAccessRights">
+                {!! Form::hidden('tokenForAjaxReq', csrf_token()) !!}
+                @csrf
+                <input name="access_route" hidden type="text" />
+                <input name="select_employee" hidden type="text" value="<?= $employee_id ?>">
+                {{-- <div style="min-height: 400px" id="dataSidebarLoader" style="">
+                    <img src="/images/loader.gif" width="30px" height="auto" style="position: absolute; left: 50%; top: 45%;">
+                </div> --}}
 
-    <div class="row" style="margin-top:30px;" id="access_rights_div" style="">
+                    <div class="header">
+                        <h2>Select Rights</h2>
+                    </div>
 
-        <div class="col-lg-6 col-md-6 col-sm-6">
-            <h2 class="_head01">Select <span> Rights</span></h2>
-        </div>
-
-        <div class="col-md-12">
-            <div class="row _checkbox-padd">
-                <?php
-                if(!$controllers->isEmpty()){
-                    foreach($controllers as $controller){ ?>
-                <div class="col-md-3 col-xs-3">
-                    <div class="custom-control custom-checkbox mr-sm-2">
-                        <input type="checkbox" class="custom-control-input routes" name="right_boxes" value="<?= $controller->route_name ?>"
-                            id="<?= $controller->route_name ?>" <?= (!$rights == "")?(array_search($controller->route_name, array_column($rights->toArray(), 'access')) !== false && array_search($controller->route_name, array_column($rights->toArray(), 'access')) >= 0) ? "checked" : "" : "" ?>>
-                        <label class="custom-control-label" for="<?= $controller->route_name ?>">
-                            <?= $controller->show_up_name ?></label>
+                    <div class="body">
+                    <div class="col-md-12">
+                        <div class="row _checkbox-padd">
+                            <?php
+                            if(!$controllers->isEmpty()){
+                                foreach($controllers as $controller){ ?>
+                            <div class="col-md-3 col-xs-3">
+                                <div class="custom-control custom-checkbox mr-sm-2">
+                                    <input type="checkbox" class="custom-control-input routes" name="right_boxes" value="<?= $controller->route_name ?>"
+                                        id="<?= $controller->route_name ?>" <?= (!$rights == "")?(array_search($controller->route_name, array_column($rights->toArray(), 'access')) !== false && array_search($controller->route_name, array_column($rights->toArray(), 'access')) >= 0) ? "checked" : "" : "" ?>>
+                                    <label class="custom-control-label" for="<?= $controller->route_name ?>">
+                                        <?= $controller->show_up_name ?></label>
+                                </div>
+                            </div>
+                            <?php }
+                            }
+                        ?>
+                        </div>
+                    </div>
+                    <div class="row body">
+                        <button style="margin-left:10px;" type="button" class="btn btn-primary mr-2 save_rights">Save</button>
                     </div>
                 </div>
-                <?php }
-                }
-            ?>
-            </div>
+                
+            </form>
         </div>
     </div>
-    <div class="row">
-        <button style="position:relative; margin-left:10px; top:25px;" type="button" class="btn btn-primary mr-2 save_rights">Save</button>
-    </div>
-</form>
+</div>
 
 @endsection
